cc3ffaf5b99482fd9ed24fa601abf9e4a74b7ee9
Davi Spindola <davispindola@protonmail.ch>
2019-01-24 23:42:46 -0200

    feat: implement list type carousel

---
+++ src/app/reusable/list/carousel/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Slider from "react-slick";
+
+/**
+ * @description Created the component
+ * @author davispindola
+ * @version 1.0
+ */
+class Carousel extends React.Component {
+  static propTypes = {
+    settings: PropTypes.object,
+    children: PropTypes.node
+  };
+
+  static defaultProps = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    className: "slider variable-width center",
+    centerMode: true,
+    variableWidth: true,
+    adaptiveHeight: true,
+    arrows: false,
+    focusOnSelect: true
+  };
+
+  render() {
+    const { settings, children } = this.props;
+
+    return (
+      <Slider {...this.props} {...settings}>
+        {children}
+      </Slider>
+    );
+  }
+}
+
+export default Carousel;
