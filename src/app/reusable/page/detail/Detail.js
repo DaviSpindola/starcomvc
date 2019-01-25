aacc5ba22409effdd9e698ff7588c726cc3a6a86
Davi Spindola <davispindola@protonmail.ch>
2019-01-25 03:59:19 -0200

    feat: implement a page component

---
+++ src/app/reusable/page/detail/Detail.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Grid, Typography, withStyles } from "@material-ui/core";
+
+import styles from "./styles";
+
+const Detail = ({ classes, title, content, children }) => (
+  <section>
+    <Grid container>
+      <Grid className={classes.item} item sm={4} xs={12} lg={6}>
+        {children}
+      </Grid>
+      <Grid className={classes.item} item sm={8} xs={12} lg={6}>
+        <div>
+          <Typography className={classes.uppercase} variant="h4" gutterBottom>
+            {title}
+          </Typography>
+          <div>
+            <Typography variant="body1">{content}</Typography>
+          </div>
+        </div>
+      </Grid>
+    </Grid>
+  </section>
+);
+
+Detail.propTypes = {
+  title: PropTypes.string,
+  content: PropTypes.string
+};
+
+export default withStyles(styles)(Detail);
---
+++ src/app/reusable/page/detail/styles.js
@@ -0,0 +1,12 @@
+const styles = () => ({
+  item: {
+    display: "flex",
+    justifyContent: "center",
+    padding: "10px 0"
+  },
+  uppercase: {
+    textTransform: "uppercase"
+  }
+});
+
+export default styles;
