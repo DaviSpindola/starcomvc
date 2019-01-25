049ce206944b5bb85a036fefc8575ef07df00a68
Davi Spindola <davispindola@protonmail.ch>
2019-01-25 16:01:30 -0200

    refactor: page layout

--- src/pages/movies/episode/styles.js
+++ src/pages/movies/episode/styles.js
@@ -2,6 +2,28 @@ const styles = () => ({
   main: {
     margin: "0 auto",
     maxWidth: "80%"
+  },
+  moreDetail: {
+    maxWidth: 300,
+    width: "100%"
+  },
+  more: {
+    display: "flex",
+    justifyContent: "flex-start"
+  },
+  item: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    padding: "10px 0px",
+    "& > span": {
+      textTransform: "uppercase",
+      color: "#ff1818",
+      fontWeight: "500"
+    },
+    "& > p": {
+      color: "#00000098"
+    }
   }
 });
 
